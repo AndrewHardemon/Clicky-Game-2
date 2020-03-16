e890c65517837c925916252eaf53d09cd787eb33
Andrew Hardemon <aeroatlas@ymail.com>
2020-03-15 22:20:15 -0700

    i think i got it up on github pages

--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  Score: require("./score")
-};
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const scoreRoutes = require("./scores");
-
-//Score Routes
-router.use("/scores", scoreRoutes);
-
-module.exports = router;
