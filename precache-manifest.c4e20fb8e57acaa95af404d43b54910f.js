d3dff715028529dcc153f61426fe7d7df0dd7276
Andrew Hardemon <aeroatlas@ymail.com>
2020-03-15 22:19:20 -0700

    Updates

---
+++ precache-manifest.c4e20fb8e57acaa95af404d43b54910f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "caf21937e3995f98a04dd730d8b1d479",
+    "url": "/Clicky-Game-2/static/media/Thumb.caf21937.png"
+  },
+  {
+    "revision": "38dafadb3b61da4f005c7634a34e2668",
+    "url": "/Clicky-Game-2/static/media/Sulfur.38dafadb.png"
+  },
+  {
+    "revision": "b6cc9afbd8ceb10f8c3176cbb059d5f4",
+    "url": "/Clicky-Game-2/static/media/Occultism.b6cc9afb.png"
+  },
+  {
+    "revision": "ac283d1ec49304f9461cb067e484e9b2",
+    "url": "/Clicky-Game-2/static/media/Face.ac283d1e.png"
+  },
+  {
+    "revision": "569bb49c922368e53826e2e969c05de8",
+    "url": "/Clicky-Game-2/static/media/Dog.569bb49c.png"
+  },
+  {
+    "revision": "78f9fe73d434521913a9",
+    "url": "/Clicky-Game-2/static/js/runtime~main.4681e127.js"
+  },
+  {
+    "revision": "e0bd52e946e29d521eea",
+    "url": "/Clicky-Game-2/static/js/main.2b1ab78d.chunk.js"
+  },
+  {
+    "revision": "d90d542fa342e9945548",
+    "url": "/Clicky-Game-2/static/js/2.1500137a.chunk.js"
+  },
+  {
+    "revision": "463774bcfe2c152288b275133e741418",
+    "url": "/Clicky-Game-2/index.html"
+  }
+];
